c6d77017137c528b21113c0dd17da968ca7f7f77
Adriano <adrianomarinho51@gmail.com>
2018-02-04 22:07:00 -0200

    Ferramentas

--- controllers/ferramentas.py
+++ controllers/ferramentas.py
@@ -11,18 +11,21 @@ def importar_produtos():
 	
 	if form.process().accepted:
 		if request.vars.csvfile != None:
-			Importar_Produtos.import_from_csv_file(request.vars.csvfile.file, delimiter=";")
-			gridProdutos = grid(Importar_Produtos,formname="importarprodutos",create=False, editable=False,
-				searchable=False,orderby = Importar_Produtos.nome)
-			
-			btnAtualizar = atualizar('atualiza_produtos','Atualizar Produtos', 'importarprodutos')
-			gridProdutos[0].insert(-1, btnAtualizar)
-			btnAtualizar["_onclick"] = "return confirm('Confirma a Atualização dos Produtos?');"
-			if btnAtualizar:
-				atualiza_produtos()
-				file = file + 'PRODUTOS ATUALIZADOS COM SUCESSO'
+			try:
+				Importar_Produtos.import_from_csv_file(request.vars.csvfile.file, delimiter=";")
+				gridProdutos = grid(Importar_Produtos,formname="importarprodutos",create=False, editable=False,
+					searchable=False,orderby = Importar_Produtos.nome)
+				
+				btnAtualizar = atualizar('atualiza_produtos',' Atualizar Produtos', 'importarprodutos')
+				gridProdutos[0].insert(-1, btnAtualizar)
+				btnAtualizar["_onclick"] = "return confirm('Confirma a Atualização dos Produtos?');"
+				if btnAtualizar:
+					atualiza_produtos()
+					response.js = "teste();"
 
-			file = 'Arquivo Carregado: %s' %(request.vars.csvfile.filename)
+				file = 'Arquivo Carregado: %s' %(request.vars.csvfile.filename)
+			except:
+				response.flash = 'Arquivo Inválido'
 
 	elif form.errors:
 		response.flash = 'Erro no Formulário'
@@ -32,12 +35,25 @@ def importar_produtos():
 def atualiza_produtos():
 	rows = db(Importar_Produtos.id > 0).select()
 	for row in rows:
+		try:
+			produto = Produtos[int(row.codigo)]
+			nome = produto.nome 
+			preco = produto.preco
+			marca = produto.marca
+			ean = produto.ean
+		except:
+			nome = marca = ean = ''
+			preco  = 0
+
 		Produtos.update_or_insert(Produtos.id == row.codigo,
                 id = row.codigo,
-                nome=row.nome,
-                estoque=row.estoque
+                nome=row.nome if row.nome else nome,
+                preco = row.preco if row.preco else preco,
+                estoque=row.estoque if row.estoque else 0,
+                marca = row.marca if row.marca else marca,
+                ean = row.ean if row.ean else ean,
                 )
-	response.js = "$('#importarprodutos').get(0).reload()"
+	
 
 
 
