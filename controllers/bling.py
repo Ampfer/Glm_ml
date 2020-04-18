3ecce157313287c3da9cd045c350c0f0786dc15e
Ampfer <adriano@ampfer.com.br>
2020-04-17 23:04:17 -0300

    ajustes

--- controllers/bling.py
+++ controllers/bling.py
@@ -24,7 +24,7 @@ def importar_vinculo():
 					preco = float(e.replace('"','').replace(',','.'))
 					preco_promocional = float(f.replace('"','').replace(',','.'))
 
-					preco_tabela = db.produtos[int(id_produto)].preco
+					preco_tabela = db.produtos[int(id_produto)].preco 
 
 					query = (Vinculos.id_bling == id_bling) & (Vinculos.id_loja == id_loja ) & (Vinculos.id_produto == id_produto) & (Vinculos.loja == loja)
 
@@ -38,22 +38,67 @@ def importar_vinculo():
 						preco_promocional = preco_promocional,
 						preco_tabela = preco_tabela
 					)
+					
 				head = True
 
 	return dict(form=form)
 
 @auth.requires_membership('admin')
+def atualizar_sugerido():
+	produtos = db(Vinculos.id > 0).select()
+	for produto in produtos:
+		Vinculos[int(produto.id)] = dict(preco=produto.preco_sugerido, preco_promocional=produto.preco_sugerido)
+	response.js = "location.reload(true)"
+
+@auth.requires_membership('admin')
+def atualizar_desconto():
+	produtos = db(Vinculos.id > 0).select()
+	for produto in produtos:
+		if produto.sugerido > 30:
+			desconto = 14
+		elif produto.sugerido > 60:
+			desconto = 16
+		elif produto.sugerido > 90:
+			desconto = 18
+		elif produto.sugerido > 120:
+			desconto = 20
+		else:
+			desconto = 12
+		Vinculos[int(produto.id)] = dict(desconto=desconto)
+		
+	response.js = "location.reload(true)"
+
+@auth.requires_membership('admin')
 def produtos_multilojas():
-    fields = (Vinculos.id_produto,Vinculos.produto, Vinculos.id_bling, Vinculos.id_loja, Vinculos.preco, Vinculos.preco_promocional, Vinculos.loja)
-    gridProdutos = grid(Vinculos,formname="formVinculo",fields=fields)
 
-    if request.args(-2) == 'new':
-       redirect(URL('produtos_multilojas'))
-    elif request.args(-3) == 'edit':
-       idVinculo = request.args(-1)
-       redirect(URL('produto_multiloja', args=idVinculo))
+	btnSugerido = A(SPAN(_class="glyphicon glyphicon-cog"), 
+		' Atualizar Sugerido ', 
+		_class="btn btn-default",
+		_id='atualizarsugerido', 
+		_onclick="if (confirm('Deseja Atualizar Preços com Sugeridos ?')) ajax('%s',[], 'formVinculo');" %URL('atualizar_sugerido',args=request.vars.keywords)
+		)
 
-    return dict(gridProdutos=gridProdutos)
+	btnDesconto = A(SPAN(_class="glyphicon glyphicon-cog"), 
+		' Atualizar Descontos ', 
+		_class="btn btn-default",
+		_id='atualizarsugerido', 
+		_onclick="if (confirm('Deseja Atualizar Descontos ?')) ajax('%s',[], 'formVinculo');" %URL('atualizar_desconto',args=request.vars.keywords)
+		)
+
+	fields = (Vinculos.id_produto,Vinculos.produto, Vinculos.id_bling, Vinculos.id_loja,Vinculos.preco_tabela, Vinculos.desconto,
+		      Vinculos.preco_sugerido,Vinculos.preco, Vinculos.preco_promocional, Vinculos.loja)
+	gridProdutos = grid(Vinculos,formname="formVinculo",fields=fields,orderby = Vinculos.produto, deletable = False, create = False, alt='300px')
+
+	gridProdutos[0].insert(-1, btnSugerido)
+	gridProdutos[0].insert(-1, btnDesconto)
+
+	if request.args(-2) == 'new':
+		redirect(URL('produtos_multilojas'))
+	elif request.args(-3) == 'edit':
+		idVinculo = request.args(-1)
+		redirect(URL('produto_multiloja', args=idVinculo))
+
+	return dict(gridProdutos=gridProdutos)
 
 @auth.requires_membership('admin')
 def produto_multiloja():
@@ -66,17 +111,22 @@ def produto_multiloja():
         btnNovo=btnExcluir=btnVoltar = ''
     else:
         formProduto = SQLFORM(Vinculos,idVinculo,_id='formProduto',field_id='id')
+        sug = Vinculos[idVinculo].preco_sugerido
 
         btnExcluir = excluir("#")
         btnNovo = novo("produto_multiloja")
 
     btnVoltar = voltar("produtos_multilojas")
 
-    if formProduto.process().accepted:
+    def validar(form):
+    	if form.vars.preco_promocional > form.vars.preco or float(form.vars.preco_promocional) < float(form.vars.preco)*0.9:
+    		form.errors.preco_promocional = 'Preço Promocional não permitido'
+
+    if formProduto.process(onvalidation=validar).accepted:
         response.flash = 'Produto Salvo com Sucesso!'
         redirect(URL('produto_multiloja', args=formProduto.vars.id))
 
     elif formProduto.errors:
         response.flash = 'Erro no Formulário Principal!'
 
-    return dict(formProduto=formProduto,btnExcluir=btnExcluir, btnVoltar=btnVoltar, btnNovo=btnNovo)
+    return dict(formProduto=formProduto, btnVoltar=btnVoltar, sug=sug)
--- models/08_bling.py
+++ models/08_bling.py
@@ -1,26 +1,36 @@
 # -*- coding: utf-8 -*-
 
+def sugerido(tabela,desconto,tarifa,frete):
+	preco_sugerido = (tabela*(1 - desconto/100))/(1 - tarifa/100) + frete
+	print round(preco_sugerido,1)
+	return preco_sugerido
+
 Vinculos = db.define_table('vinculos',
 	Field('id_produto', 'string', label='Id Produto:', length = 30),
 	Field('produto', 'string', label='Produto:', length = 60),
 	Field('id_bling', 'string', label='Id Bling:', length = 30),
 	Field('id_loja', 'string', label='Id Loja:', length = 30),
-	Field('loja',	'string', label = 'Loja', length = 30),
+	Field('loja', 'string', label = 'Loja', length = 30),
 	Field('desconto','decimal(7,2)',label='Desconto:'),
 	Field('tarifa','decimal(7,2)',label='Tarifa:'),
-	Field('Frete','decimal(7,2)',label='Frete:'),
+	Field('frete','decimal(7,2)',label='Frete:'),
 	Field('preco','decimal(7,2)',label='Preço:'),
 	Field('preco_promocional','decimal(7,2)',label='Promocional:'),
 	Field('preco_tabela','decimal(7,2)',label='Preco Tabela:'),
+	Field.Virtual('preco_sugerido',lambda row: round(sugerido( float(row.vinculos.preco_tabela), float(row.vinculos.desconto), float(row.vinculos.tarifa), float(row.vinculos.frete)),1),label='Preco Sugerido:')
     )
 
 Vinculos.desconto.requires = IS_EMPTY_OR(IS_DECIMAL_IN_RANGE(dot=','))
-Vinculos.desconto.default = 15
+Vinculos.desconto.default = 12
 Vinculos.tarifa.requires = IS_EMPTY_OR(IS_DECIMAL_IN_RANGE(dot=','))
 Vinculos.tarifa.default = 11
-Vinculos.Frete.requires = IS_EMPTY_OR(IS_DECIMAL_IN_RANGE(dot=','))
+Vinculos.frete.requires = IS_EMPTY_OR(IS_DECIMAL_IN_RANGE(dot=','))
+Vinculos.frete.default = 5
 Vinculos.preco.requires = IS_EMPTY_OR(IS_DECIMAL_IN_RANGE(dot=','))
 Vinculos.preco_promocional.requires = IS_EMPTY_OR(IS_DECIMAL_IN_RANGE(dot=','))
+Vinculos.id_bling.writable = False
+Vinculos.id_loja.writable = False
+
 
 
 
